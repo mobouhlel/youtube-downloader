bd4251096ed57d457c051bdabc8aac38bd852ed5
Yadnesh Sawant <hsenday.6991@gmail.com>
2017-07-23 07:25:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+var ytdl = require('ytdl-core');
+
+var app = express();
+
+app.listen(3000,function(){
+    console.log('server->on');
+});
+
+app.get('/downloader',function(req,res){
+    res.sendFile(__dirname + '/front-end.html');
+});
+
+var link;
+app.post('/downloader/success',urlencodedParser,function(req,res){
+
+        ytdl(JSON.stringify(req.body)).pipe(fs.createWriteStream('video.mp4'));
+        res.sendFile(__dirname+'/success-page.html');
+});
